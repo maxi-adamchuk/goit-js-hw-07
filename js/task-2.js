4a08732ebd4099b55ce8b7b76553ebc3de7198ef
maxi-adamchuk <167241929+maxi-adamchuk@users.noreply.github.com>
2024-10-08 00:46:54 +0300

    tasks 1-2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,9 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = images
+.map(({ url, alt }) => `<li><img src="${url}" alt="${alt}" width="400"></li>`)
+.join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
